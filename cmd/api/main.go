3591e8f31a2bcbe8c2a0f2b375cbb5c6dbf7f967
shortyk <shortykofficial@gmail.com>
2025-04-14 20:25:30 +0300

    added initializing connection pool for postgresql

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+	_ "github.com/joho/godotenv/autoload"
 )
 
 const version = "1.0.0"
@@ -15,6 +19,9 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn string
+	}
 }
 
 type application struct {
@@ -27,9 +34,19 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "Api server port")
 	flag.StringVar(&cfg.env, "env", "development", "Enviroment (development|staging|production)")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
+
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
+	logger.Info("database connection pool established")
 
 	app := newApplication().
 		setConfig(cfg).
@@ -44,7 +61,7 @@ func main() {
 	}
 
 	app.logger.Log(context.Background(), slog.LevelInfo, "starting server", "addr", server.Addr, "env", cfg.env)
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	app.logger.Error(err.Error())
 	os.Exit(1)
 }
@@ -61,4 +78,21 @@ func (app *application) setConfig(cfg config) *application {
 func (app *application) setLogger(log *slog.Logger) *application {
 	app.logger = log
 	return app
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("pgx", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err = db.PingContext(ctx); err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }

21089a488f90870e4e8a345f2dcfe0b959995dcd
shortyk <shortykofficial@gmail.com>
2025-04-19 20:40:54 +0300

    updated query in GetAll method and added additional arguments in QueryContext to support filtering by title and genres

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -163,12 +163,14 @@ func (m MovieModel) GetAll(title string, genres Genres, filters Filters) ([]*Mov
 	query := `
 		SELECT created_at, title, year, runtime, genres, version
 		FROM movies
+		WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '')
+		AND (genres @> $2 OR $2 = '{}')
 		ORDER BY id`
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
-	rows, err := m.DB.QueryContext(ctx, query)
+	rows, err := m.DB.QueryContext(ctx, query, title, genres)
 	if err != nil {
 		return nil, err
 	}

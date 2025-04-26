b39c63e34b5d6d7a56b44c5c5c4a987d0376833a
shortyk <shortykofficial@gmail.com>
2025-04-26 23:15:56 +0300

    added base movies:read permission to all registered users

--- internal/data/permissions.go
+++ internal/data/permissions.go
@@ -50,3 +50,15 @@ func (m PermissionModel) GetAllForUser(userID int64) (permissions Permissions, e
 
 	return
 }
+
+func (m PermissionModel) AddForUser(userID int64, codes ...string) error {
+	query := `
+		INSERT INTO users_permissions
+		SELECT $1, permissions.id FROM permissions WHERE permissions.code = ANY($2)`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	_, err := m.DB.ExecContext(ctx, query, userID, codes)
+	return err
+}

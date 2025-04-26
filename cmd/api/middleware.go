5523de8049df52b133802a9f16db9a29ebdce169
shortyk <shortykofficial@gmail.com>
2025-04-27 02:03:36 +0300

    added preflight requests support to Cors middleware

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -142,10 +142,18 @@ func (app *application) requirePermission(next http.HandlerFunc, code string) ht
 func (app *application) enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Vary", "Origin")
+		w.Header().Add("Vary", "Access-Control-Request-Method")
 
 		origin := r.Header.Get("Origin")
 		if origin != "" && slices.Contains(app.config.cors.trustedOrigins, origin) {
 			w.Header().Set("Access-Control-Allow-Origin", origin)
+			if r.Method == http.MethodOptions && r.Header.Get("Access-Control-Request-Method") != "" {
+				w.Header().Set("Access-Control-Allow-Methods", "OPTIONS, PUT, PATCH, DELETE")
+				w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
+
+				w.WriteHeader(http.StatusOK)
+				return
+			}
 		}
 
 		next.ServeHTTP(w, r)

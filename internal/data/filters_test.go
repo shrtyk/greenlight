87c254248a572f544b58721ae37adae79cdf9d00
shortyk <shortykofficial@gmail.com>
2025-05-06 21:28:21 +0300

    added filters tests

---
+++ internal/data/filters_test.go
@@ -0,0 +1,50 @@
+package data
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/shortykevich/greenlight/internal/validator"
+)
+
+func TestFilters(t *testing.T) {
+	filters := Filters{
+		Page:     1,
+		PageSize: 20,
+		Sort:     "-title",
+		SortSafelist: []string{
+			"id", "title", "year", "runtime",
+			"-id", "-title", "-year", "-runtime",
+		},
+	}
+
+	v := validator.New()
+
+	filters.Validate(v)
+	if !v.Valid() {
+		t.Errorf("expected to get valid filters but got: %v", v.Errors)
+	}
+
+	gotSortCol := filters.sortColumn()
+	if gotSortCol != "title" {
+		t.Errorf("got: %s, want: %s", gotSortCol, "title")
+	}
+
+	gotSortDir := filters.sortDirection()
+	if gotSortDir != "DESC" {
+		t.Errorf("got: %s, want: %s", gotSortCol, "DESC")
+	}
+
+	totalRecs := 5
+	gotMeta := calculateMetadata(totalRecs, filters.Page, filters.PageSize)
+	wantMeta := Metadata{
+		CurrentPage:  filters.Page,
+		PageSize:     filters.PageSize,
+		FirstPage:    1,
+		LastPage:     1,
+		TotalRecords: totalRecs,
+	}
+	if !reflect.DeepEqual(gotMeta, wantMeta) {
+		t.Errorf("got: %+v, want: %+v", gotMeta, wantMeta)
+	}
+}
--- internal/validator/validator_test.go
+++ internal/validator/validator_test.go
@@ -7,21 +7,34 @@ import (
 )
 
 func TestValidator(t *testing.T) {
-	cases := map[string]string{
-		"right-email": "asd@gmail.com",
-		"wrong-email": "!das@gmail.com",
-		"password":    "v",
-	}
+	t.Run("validations test", func(t *testing.T) {
+		cases := map[string]string{
+			"right-email": "asd@gmail.com",
+			"wrong-email": "!das@gmail.com",
+			"password":    "v",
+		}
 
-	v := validator.New()
-	v.Check(validator.Matches(cases["right-email"], validator.EmailRX), "email", "wrong email")
-	if !v.Valid() {
-		t.Errorf("values should be valid. Errors: %+v", v.Errors)
-	}
+		v := validator.New()
+		v.Check(validator.Matches(cases["right-email"], validator.EmailRX), "email", "wrong email")
+		if !v.Valid() {
+			t.Errorf("values should be valid. Errors: %+v", v.Errors)
+		}
 
-	v.Check(validator.Matches(cases["wrong-email"], validator.EmailRX), "email", "wrong email")
-	v.Check(len(cases["short-val"]) >= 8, "password", "password length should longer than 8 characters")
-	if v.Valid() {
-		t.Errorf("values shouldn't be valid at this point. Errors: %+v", v.Errors)
-	}
+		v.Check(validator.Matches(cases["wrong-email"], validator.EmailRX), "email", "wrong email")
+		v.Check(len(cases["short-val"]) >= 8, "password", "password length should longer than 8 characters")
+		if v.Valid() {
+			t.Errorf("values shouldn't be valid at this point. Errors: %+v", v.Errors)
+		}
+	})
+
+	t.Run("uniqueness test", func(t *testing.T) {
+		uniques := []string{"a", "b", "c"}
+		nonUniques := []int{1, 2, 2, 3}
+		if !validator.Unique(uniques) {
+			t.Errorf("got: %v, want: %v", validator.Unique(uniques), true)
+		}
+		if validator.Unique(nonUniques) {
+			t.Errorf("got: %v, want: %v", true, validator.Unique(uniques))
+		}
+	})
 }
